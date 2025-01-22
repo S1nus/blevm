b0e4b868e63f2a3d9bee19fbc93d2c0353a20133
c-node <chuckhead7@gmail.com>
2025-01-22 18:49:26 -0500

    create input type

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,4 +1,23 @@
+use celestia_types::{nmt::Namespace, nmt::NamespaceProof, AppVersion, Blob};
+use nmt_rs::{simple_merkle::proof::Proof, NamespacedHash, TmSha2Hasher};
+use rsp_client_executor::{
+    io::ClientExecutorInput, ChainVariant, ClientExecutor, EthereumVariant, CHAIN_ID_ETH_MAINNET,
+    CHAIN_ID_LINEA_MAINNET, CHAIN_ID_OP_MAINNET,
+};
 use serde::{Deserialize, Serialize};
+use tendermint::Hash as TmHash;
+
+#[derive(Serialize, Deserialize)]
+pub struct BlevmInput {
+    pub input: ClientExecutorInput,
+    pub namespace: Namespace,
+    pub celestia_header_hash: TmHash,
+    pub data_hash: TmHash,
+    pub proof_data_hash_to_celestia_hash: Proof<TmSha2Hasher>,
+    pub row_root_multiproof: Proof<TmSha2Hasher>,
+    pub nmt_multiproofs: Vec<NamespaceProof>,
+    pub row_roots: Vec<NamespacedHash<29>>,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct BlevmOutput {
